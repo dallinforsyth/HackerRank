08a35cab766c4f71d8463ceb9f64737478f2265f
dallinforsyth <dallfor192@gmail.com>
2023-04-20 15:58:53 -0500

    completed grading studnets, with help from fourms

--- algorithm.js
+++ algorithm.js
@@ -277,15 +277,16 @@
 // }
 // console.log(timeConversion("12:00:00PM"));
 
-// function gradingStudents(grades) {
-//   for (let i = 0; i < grades.length; i++) {
-//     if (grades[i] < 37) {
-//       break;
-//     } else if (grades[i] % 100) {
-//       grades[i] + 2;
-//     }
-//   }
-//   return grades;
-// }
+function gradingStudents(grades) {
+  // Write your code here
+  for (let i = 0; i < grades.length; i++) {
+    if ((grades[i] + 2) % 5 == 0 && grades[i] + 2 > 39) {
+      grades[i] = grades[i] + 2;
+    } else if ((grades[i] + 1) % 5 == 0 && grades[i] + 2 > 39) {
+      grades[i] = grades[i] + 1;
+    }
+  }
+  return grades;
+}
 
-// console.log(gradingStudents([73, 67, 38, 33]));
+console.log(gradingStudents([73, 67, 38, 33]));
