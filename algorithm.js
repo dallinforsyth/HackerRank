3e755178285d8dbedb56017536d8df20e50150da
dallinforsyth <dallfor192@gmail.com>
2023-07-02 17:51:56 -0500

    new

--- algorithm.js
+++ algorithm.js
@@ -340,21 +340,21 @@
 //   // Write your code hereeeeee
 // }
 
-function breakingRecords(scores) {
-  var max = scores[0];
-  var min = scores[0];
-  var maxCount = 0;
-  var minCount = 0;
-  for (let i = 0; i < scores.length; i++) {
-    if (scores[i] > max) {
-      maxCount += 1;
-      max = scores[i];
-    } else if (scores[i] < min) {
-      minCount += 1;
-      min = scores[i];
-    }
-  }
-  console.log(maxCount, minCount);
-}
+// function breakingRecords(scores) {
+//   var max = scores[0];
+//   var min = scores[0];
+//   var maxCount = 0;
+//   var minCount = 0;
+//   for (let i = 0; i < scores.length; i++) {
+//     if (scores[i] > max) {
+//       maxCount += 1;
+//       max = scores[i];
+//     } else if (scores[i] < min) {
+//       minCount += 1;
+//       min = scores[i];
+//     }
+//   }
+//   console.log(maxCount, minCount);
+// }
 
-breakingRecords([10, 5, 20, 20, 4, 5, 2, 25, 1]);
+// breakingRecords([10, 5, 20, 20, 4, 5, 2, 25, 1]);
