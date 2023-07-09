d68b5828e4413ccf293e979edbe7aef08311387c
dallinforsyth <dallfor192@gmail.com>
2023-07-09 14:39:36 -0500

    1

--- algorithm.js
+++ algorithm.js
@@ -360,7 +360,7 @@
 // breakingRecords([10, 5, 20, 20, 4, 5, 2, 25, 1]);
 
 function getTotalX(a, b) {
-  // Write your code hereee
+  // Write your code hereeee
 }
 
 getTotalX([2, 4], [16, 32, 96]);
