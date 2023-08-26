1556531e9a6ef7fff817117c0237162a6111d676
dallinforsyth <dallfor192@gmail.com>
2023-08-26 18:06:17 -0500

    ue

--- algorithm.js
+++ algorithm.js
@@ -286,7 +286,7 @@
 //       grades[i] = grades[i] + 1;
 //     }
 //   }
-//   retursdssedfsedawn grades;
+//   retursdsssdedfsedawn grades;
 // }
 
 // console.log(gradingStudents([73, 67, 38, 33]));
